55a6a871eb31c7bf7aacbcf133f9dbd20c8841ef
topbrightdev <topbrightdev@gmail.com>
2020-01-22 22:34:46 +0200

    added fix version

--- src/components/shoe-shop/Shoes-shop.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ShoesShop } from './Shoes-shop';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ShoesShop />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
